72dd43758c8390aca10cccf6d59f21e05940285f
gentil-eilison <fernandes.gentilalysonjac@gmail.com>
2024-04-10 11:26:12 -0300

    feature: adds booking client for grpc communication

---
+++ src/clients/booking.js
@@ -0,0 +1,17 @@
+const grpc = require('@grpc/grpc-js');
+const protoLoader = require('@grpc/proto-loader');
+
+const packageDefinition = protoLoader.loadSync('../proto/bookings.proto', {
+    keepCase: true,
+    longs: String,
+    enums: String,
+    defaults: false,
+    arrays: false,
+    objects: false,
+    oneofs: false,
+});
+
+const BookingController = grpc.loadPackageDefinition(packageDefinition).hotel_rooms.bookings.BookingController;
+const client = new BookingController('127.0.0.1:50052', grpc.credentials.createInsecure());
+
+module.exports = client;
