4b48c8decc3f017dbf089a50553309d97fc307c3
KarlaJulyana <karla@solarz.com.br>
2024-04-12 13:33:38 -0300

    fix: change booking port

--- src/clients/bookings.js
+++ src/clients/bookings.js
@@ -3,7 +3,7 @@ const Client = require("./ClientMaker");
 const client = new Client(
   "../proto/bookings.proto",
   "hotel_rooms.bookings.BookingController",
-  "127.0.0.1:50051",
+  "127.0.0.1:50052",
   {
     keepCase: true,
     longs: String,
